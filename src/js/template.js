1aa1738fac77bbd4c2993fcf9924d3cf01441af0
Sue Roberts <99991896+SueRtx@users.noreply.github.com>
2022-05-20 09:39:03 -0500

    Initial commit

---
+++ src/js/template.js
@@ -0,0 +1,19 @@
+/*export default class temp {
+  constructor(side1, side2, side3) {
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+  }
+
+  checkType() {
+    if ( ) {
+      return " ";
+    } else if ( ) {
+      return " ";
+    }  else if ( ) {
+      return " ";
+    } else {
+      return " ";
+    }
+  }    
+}*/
