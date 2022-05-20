790bae9e424971ee74c921b7d74fd83a88d71c63
Sue Roberts <suerobertstx@yahoo.com>
2022-05-20 12:11:01 -0500

    Add test 3: calculate user age on the planet.

--- __tests__/planet.test.js
+++ __tests__/planet.test.js
@@ -1,17 +1,25 @@
 import HumanAge from '../src/js/planet.js';
 
 describe('HumanAge', () => {
+  let humanAge
+  beforeEach(() =>{
+    humanAge = humanAge = new HumanAge(30, 90);
+  })
 
   test("should correctly create a humanAge object", () => {
-    const humanAge = new HumanAge(30, 90);
     expect(humanAge.ageNow).toEqual(30);
     expect(humanAge.ageEnd).toEqual(90);
   });
 
   test("should correctly calculate user age on the Mercury", () => {
-    const humanAge = new HumanAge(30, 90);
     expect(humanAge.planetAge("mercury")).toEqual(125);
-    
+  });
+
+  test("should correctly calculate user age on the planet", () => {
+    expect(humanAge.planetAge("venus")).toEqual(48);
+    expect(humanAge.planetAge("mars")).toEqual(16);
+    expect(humanAge.planetAge("jupiter")).toEqual(3);
+    expect(humanAge.planetAge("sun")).toEqual("");
   });
   
 
