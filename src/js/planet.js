08009c4c511f48af0721763728b11d7736e0d321
Sue Roberts <suerobertstx@yahoo.com>
2022-05-20 12:16:35 -0500

    Pass test 3

--- src/js/planet.js
+++ src/js/planet.js
@@ -7,9 +7,13 @@ export default class HumanAge {
 
   planetAge(planet) {
     if (planet === "mercury" ) {
-      return (Math.round(this.ageNow/.24));
-   // } else if ( ) {
-   //   return " ";
+      return (Math.round(this.ageNow / .24));
+    } else if (planet === "venus" ) {
+      return (Math.round(this.ageNow / .62));;
+    } else if (planet === "mars" ) {
+      return (Math.round(this.ageNow / 1.88));;
+    } else if (planet === "jupiter" ) {
+      return (Math.round(this.ageNow / 11.86));;
    
     } else {
       return "";
